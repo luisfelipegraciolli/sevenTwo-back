0c5fd1ebd9afc72a7e3e0586084dc906431e458d
Luis Felipe Graciolli <shiwa101@proton.me>
2024-09-02 17:51:30 -0300

    feat: Muda metodo de request do login para POST

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
+    const authHeader = req.headers["Authorization"];
     const token = authHeader && authHeader.split(" ")[1];
     if (!token) return res.sendStatus(401);
     try {
