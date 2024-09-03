cb833a5eed71871b8a8690d421a82eca3796d829
Luis Felipe Graciolli <shiwa101@proton.me>
2024-09-03 17:37:50 -0300

    feat: adiciona rota exclusiva para verificação de Token JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 var express = require("express")
 var router = express.Router()
 const authController = require("../controllers/empresasController")
+const authMiddleware = require("../middlewares/authMiddleware")
 
 router.post("/login", authController.login)
+router.post("/jwt", authMiddleware.authenticateToken)
 
 module.exports = router
