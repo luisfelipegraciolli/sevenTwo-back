1104b0dbe730eaa797e0240f5c089e7689128316
Luis Felipe Graciolli <shiwa101@proton.me>
2024-09-04 11:44:19 -0300

    style: segue configs do prettier atual

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
-var express = require("express")
-var router = express.Router()
-const authController = require("../controllers/empresasController")
-const authMiddleware = require("../middlewares/authMiddleware")
+var express = require("express");
+var router = express.Router();
+const authController = require("../controllers/empresasController");
 
-router.post("/login", authController.login)
+router.post("/login", authController.login);
 
-module.exports = router
+module.exports = router;
